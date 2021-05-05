1024dc445aafe7658afd0202b137e3d2d2d2e344
Ebeling, Dr. Bastian <bastian.ebeling@web.de>
2021-05-05 08:44:58 +0200

    Leerzeilen gelöscht für Filter-Test

--- py/bastipy.py
+++ py/bastipy.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3 -OO
 """bastipy is just a module with some helpers for me """
 from typing import Callable, TypeVar
-
-
 def ic(*a):  # pylint: disable=invalid-name
     """Just in case package icecream is not available: For logging purpuses."""
     if not a:
         return None
     return a[0] if len(a) == 1 else a
-
-
 if __debug__:
     try:
         from icecream import ic  # type: ignore[import,no-redef]  # noqa: F811,W0404
@@ -22,7 +18,6 @@ try:
 except ImportError:
     pass
 else:
-
     def loadonecore(
         loadduration: int = 10, loadedcore: int = 0, theload: float = 0.5
     ) -> None:
@@ -32,33 +27,25 @@ else:
             duration_s=loadduration,
             target_load=theload,
         )
-
-
 try:
     from cpu_load_generator import load_all_cores  # type: ignore[import]
 except ImportError:
     pass
 else:
-
     def loadallcores(loadduration: int = 10, theload: float = 0.5) -> None:
         """just a helper function to generate load on all cores"""
         load_all_cores(duration_s=loadduration, target_load=theload)
-
-
 InnerFunctionReturnType = TypeVar("InnerFunctionReturnType")
 try:
     import psutil  # type: ignore[import]
 except ImportError:
     import os
-
     ic("Timing of function with the help of module os")
-
     def bastitiming(
         func: Callable[..., InnerFunctionReturnType]
     ) -> Callable[..., InnerFunctionReturnType]:
         """bastitiming is a decorator to time calls"""
         savename = func.__name__
-
         def wrapped(*args, **kwargs):
             beforesys = os.times()
             retval = func(*args, **kwargs)
@@ -68,19 +55,14 @@ except ImportError:
             )
             print(f"processing of {savename} took {thediffsys}")
             return retval
-
         return wrapped
-
-
 else:
     ic("Timing of function with the help of module psutil")
-
     def bastitiming(
         func: Callable[..., InnerFunctionReturnType]
     ) -> Callable[..., InnerFunctionReturnType]:
         """bastitiming is a decorator to time calls"""
         savename = func.__name__
-
         def wrapped(*args, **kwargs):
             beforesys = psutil.cpu_times()
             before = psutil.Process().cpu_times()
@@ -99,8 +81,6 @@ else:
             return retval
 
         return wrapped
-
-
 if __name__ == "__main__":
     print("DANKE Basti")
     bastitiming(loadallcores)(15, 0.9)
